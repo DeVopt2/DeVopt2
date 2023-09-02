5a56ef60cb97f988ceb07eb8b80cee88253a84ff
@DeVopt2 <137735813+DeVopt2@users.noreply.github.com>
2023-09-02 11:02:04 -0700

    Update Population.c

--- Population.c
+++ Population.c
@@ -1,4 +1,4 @@
-include <cs50.h>
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
@@ -17,31 +17,28 @@ int main(void)
     int stn, enn;
 
     // printf statement
-    printf("This Program Calculates Years To Reach A Given Population Of Lammas based on yearly relative frequency distributions "
-           "of: birth %f mortality %f\n",
-           b, m);
+    printf("This Program Calculates Years To Reach An Entered Population Of Lamma based on yearly relative frequency distributions of: birth %f mortality %f\n",b, m);
 
     do
     {
-        stn = get_int("LAMMA STARTING POPULATION SIZE:Enter A Whole Number 9 Or Higher:   ");
+        stn = get_int("LAMMA STARTING POPULATION NUMBER:Enter A Whole Number 9 Or Higher:   ");
     }
 
     while (stn < 9);
 
     do
     {
-        enn = get_int("LAMMA ENDING POPULATION SIZE:Enter A Whole Number Equal To Or Higher Than Your Starting Number:   ");
+        enn = get_int("LAMMA ENDING POPULATION NUNBER:Enter A Whole Number Equal To Or Higher Than Your Starting Number:   ");
     }
     while (enn < stn);
 
-    /*calculating years to reach end
-     population number based on yearly birth and mortality percentages*/
+    /*calculating years to reach population number based on yearly birth and mortality percentages*/
 
-    float y1 = stn + (float) stn / 3 - (float) stn / 4;
-    float y2 = (float) y1 * (enn - stn);
+    float y1 = (float) stn / 3 - (float) stn / 4;
+    float y2 = (float) (enn - stn)/y1;
 
-    // years for Lamma to reach a given number of live births
-    printf("Years For Lamma To Reach A Give Number Of Live Births: %f\n", y1 + y2);
+    // Years to reach ending number
+    printf("Years: %f\n", y2);
 
     return 0;
 }
